d4ec80e105f08f1c5af47ee6d2e40ad2eed5ac5e
John Anthony Rico <johnrico364@gmail.com>
2024-11-06 13:23:42 +0800

    Add: Database table and seeder

---
+++ server/database/migrations/2024_11_06_022046_create_recipes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('recipes', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->longText('description')->nullable();
+            $table->json('category')->nullable();
+            $table->json('ingredients')->nullable();
+            $table->string('country')->nullable();
+            $table->integer('prep_time')->nullable();
+            $table->string('yt_link')->nullable();
+            $table->string('image')->default('default.png');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('recipes');
+    }
+};
