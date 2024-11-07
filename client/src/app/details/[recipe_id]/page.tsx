eba57d21d23b1dd09e1c9280349d6ac9f48c486f
John Anthony Rico <johnrico364@gmail.com>
2024-11-07 12:59:33 +0800

    Update: Make Recipe Deatails semi dynamic

--- client/src/app/details/[recipe_id]/page.tsx
+++ client/src/app/details/[recipe_id]/page.tsx
@@ -1,7 +1,8 @@
 /* eslint-disable @typescript-eslint/no-require-imports */
 "use client";
+import { useGetRecipes } from "@/hooks/useGetRecipes";
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 // Icons
 import { AiOutlineAppstoreAdd } from "react-icons/ai";
@@ -13,27 +14,49 @@ type Props = {
     recipe_id: string;
   };
 };
+type RecipeProps = {
+  category: string;
+  country: string;
+  description: string;
+  id: number;
+  image: string;
+  ingredients: string;
+  name: string;
+  prep_time: number;
+  yt_link: string;
+};
 
 const ingredients = ["Egg", "Pork", "Soy Sauce", "Vinegar", "Black Pepper"];
 
 export default function RecipeDetails({ params }: Props) {
+  const { getRecipeDetails } = useGetRecipes();
+
+  const [recipeDetails, set_recipeDetails] = useState<RecipeProps>();
   const [pageNumber, set_pageNumber] = useState(0);
   const pageVisited = pageNumber * 3;
 
   const displayIngredients = ingredients.slice(pageVisited, pageVisited + 3);
 
-  console.log(displayIngredients.length);
-
   const swipeIngredientsFn = (action: string) => {
     const isNext = displayIngredients.length === 3;
     const isBack = pageNumber !== 0;
     if (action === "next" && isNext) {
-      set_pageNumber(pageNumber + 1)
+      set_pageNumber(pageNumber + 1);
     }
     if (action === "back" && isBack) {
-      set_pageNumber(pageNumber - 1)
+      set_pageNumber(pageNumber - 1);
     }
   };
+
+  const effectFn = async () => {
+    const response = await getRecipeDetails(params.recipe_id);
+    console.log(response);
+    set_recipeDetails(response);
+  };
+
+  useEffect(() => {
+    effectFn();
+  }, []);
 
   return (
     <div className="recipe-details-section">
@@ -48,7 +71,7 @@ export default function RecipeDetails({ params }: Props) {
               />
             </div>
             <div className="basis-6/12 px-10">
-              <div className="recipe-name">Adobo Sinigang</div>
+              <div className="recipe-name">{recipeDetails?.name}</div>
 
               <div className="wrapper mt-9">
                 <div className="recipe-details">
@@ -56,7 +79,7 @@ export default function RecipeDetails({ params }: Props) {
                     <AiOutlineAppstoreAdd className="icon" />
                   </div>
                   <div className="flex justify-center">
-                    <div className="value">Main Course</div>
+                    <div className="value">{recipeDetails?.category[0]}</div>
                   </div>
                 </div>
                 <div className="recipe-details">
@@ -64,7 +87,9 @@ export default function RecipeDetails({ params }: Props) {
                     <FaRegClock className="icon" />
                   </div>
                   <div className="flex justify-center">
-                    <div className="value">4 Hours</div>
+                    <div className="value">
+                      {recipeDetails?.prep_time} minutes
+                    </div>
                   </div>
                 </div>
                 <div className="recipe-details">
@@ -72,45 +97,14 @@ export default function RecipeDetails({ params }: Props) {
                     <BiWorld className="icon" />
                   </div>
                   <div className="flex justify-center">
-                    <div className="value">India</div>
+                    <div className="value">{recipeDetails?.country}</div>
                   </div>
                 </div>
                 <div className="border-2 mt-16 w-full"></div>
               </div>
 
               <div className="recipe-description">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Repellat doloribus laborum hic ducimus autem, cupiditate
-                exercitationem voluptatibus quisquam tenetur sapiente excepturi
-                facilis sequi voluptate earum est illo eaque sunt ipsa. Lorem
-                ipsum dolor, sit amet consectetur adipisicing elit. Consectetur,
-                natus libero omnis recusandae similique unde, doloribus velit a
-                nemo earum magnam accusamus hic nihil et cupiditate vero nostrum
-                esse quia! Lorem ipsum dolor sit amet consectetur adipisicing
-                elit. Quaerat quidem totam iste enim explicabo maiores
-                doloremque aperiam eos aliquam maxime ratione molestias dicta
-                ullam autem esse harum culpa dolor similique recusandae ea,
-                nihil cupiditate consequuntur sit. Autem delectus perspiciatis
-                voluptatibus iure labore dignissimos illo exercitationem sint
-                modi quod, quis commodi ut veniam. Ullam labore quos modi
-                quibusdam rem sit praesentium. Recusandae eius ea officia aut
-                saepe quisquam esse pariatur! Esse commodi, blanditiis quaerat
-                veniam ipsa natus a exercitationem hic atque illo provident
-                deserunt necessitatibus. Totam sequi dolorem perferendis sunt
-                dicta laboriosam eos! Voluptatem molestias est repellendus
-                aspernatur illum dignissimos quod, asperiores, nihil nemo culpa
-                rerum laborum magnam sapiente maiores alias a? Sed esse
-                dignissimos exercitationem rem beatae dicta maxime ex hic iste,
-                at accusantium sit ut assumenda, suscipit fuga distinctio
-                praesentium quidem optio modi mollitia eum vel. Maiores dolores
-                quidem pariatur quaerat tempore quasi possimus, omnis
-                repudiandae? Aperiam consequuntur explicabo omnis quia
-                asperiores aut obcaecati quo quae neque dolor. Iure doloremque
-                voluptatibus atque nulla fugit culpa error temporibus non a
-                aspernatur adipisci hic delectus debitis esse sint ad ab
-                explicabo, harum architecto facere dolor itaque odio aperiam
-                possimus. Cumque numquam asperiores quia praesentium quos dolore
-                consectetur vitae omnis, velit nulla?
+                {recipeDetails?.description}
               </div>
             </div>
           </div>
@@ -152,7 +146,12 @@ export default function RecipeDetails({ params }: Props) {
             </div>
             <div className="lg:basis-6/12 md:basis-5/12">
               <div className="wrapper justify-end">
-                <button className="tutorial-btn">Show Tutorial</button>
+                <button
+                  className="tutorial-btn"
+                  onClick={() => window.open(recipeDetails?.yt_link, '_blank')}
+                >
+                  Show Tutorial
+                </button>
               </div>
             </div>
           </div>
--- client/src/hooks/useGetRecipes.ts
+++ client/src/hooks/useGetRecipes.ts
@@ -6,11 +6,34 @@ export const useGetRecipes = () => {
       const data = await axios.get(
         `http://127.0.0.1:8000/api/recipes/category/${category}`
       );
-      return data?.data?.data
+      return data?.data?.data;
     } catch (error) {
       console.log(error);
     }
   };
 
-  return { getRecipesByCategory };
+  const getRecipeDetails = async (id: string) => {
+    try {
+      const data = await axios.get(
+        `http://127.0.0.1:8000/api/recipe_details/${id}`
+      );
+      const details = data?.data?.data;
+      const recipeReturn = {
+        category: JSON.parse(details.category),
+        country: details.country,
+        description: details.description,
+        id: details.id,
+        image: details.image,
+        ingredients: JSON.parse(details.ingredients),
+        name: details.name,
+        prep_time: details.prep_time,
+        yt_link: details.yt_link,
+      };
+      return recipeReturn;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return { getRecipesByCategory, getRecipeDetails };
 };
