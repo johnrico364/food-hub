718a52e00fec825d04c33aa7483ac67db0f36a16
John Anthony Rico <johnrico364@gmail.com>
2024-11-08 22:07:43 +0800

    Updata: Import categories and remove the inside categories

--- client/src/app/recipes/page.tsx
+++ client/src/app/recipes/page.tsx
@@ -6,78 +6,71 @@ import { CategoryBox } from "@/components/category-box";
 import { RecipeBox } from "@/components/recipe-box";
 // Hooks
 import { useGetRecipes } from "@/hooks/useGetRecipes";
+import { categories } from "@/hooks/categories";
 // Icons
-import { AiOutlineAppstoreAdd } from "react-icons/ai";
-import { CiBowlNoodles } from "react-icons/ci";
-import { IoFishOutline, IoRestaurantOutline, IoSearch } from "react-icons/io5";
-import { LiaMugHotSolid } from "react-icons/lia";
-import { LuIceCream2, LuPopcorn, LuSalad, LuSoup } from "react-icons/lu";
-import { MdOutlineBakeryDining } from "react-icons/md";
-import { BiBowlRice } from "react-icons/bi";
+import { IoSearch } from "react-icons/io5";
 
 type RecipeProps = {
   id: number;
   name: string;
   description: string;
 };
-
-const categories = [
-  {
-    icon: AiOutlineAppstoreAdd,
-    name: "All",
-    items: 0,
-  },
-  {
-    icon: IoRestaurantOutline,
-    name: "Main course",
-    items: 0,
-  },
-  {
-    icon: LiaMugHotSolid,
-    name: "Breakfast",
-    items: 0,
-  },
-  {
-    icon: LuSoup,
-    name: "Soups",
-    items: 0,
-  },
-  {
-    icon: CiBowlNoodles,
-    name: "Pasta",
-    items: 0,
-  },
-  {
-    icon: LuIceCream2,
-    name: "Desserts",
-    items: 0,
-  },
-  {
-    icon: LuSalad,
-    name: "Salad",
-    items: 0,
-  },
-  {
-    icon: MdOutlineBakeryDining,
-    name: "Baked",
-    items: 0,
-  },
-  {
-    icon: LuPopcorn,
-    name: "Snacks",
-    items: 0,
-  },
-  {
-    icon: BiBowlRice,
-    name: "Appetizers",
-    items: 0,
-  },
-  {
-    icon: IoFishOutline,
-    name: "Seafood",
-    items: 0,
-  },
-];
+//   {
+//     icon: AiOutlineAppstoreAdd,
+//     name: "All",
+//     items: 0,
+//   },
+//   {
+//     icon: IoRestaurantOutline,
+//     name: "Main course",
+//     items: 0,
+//   },
+//   {
+//     icon: LiaMugHotSolid,
+//     name: "Breakfast",
+//     items: 0,
+//   },
+//   {
+//     icon: LuSoup,
+//     name: "Soups",
+//     items: 0,
+//   },
+//   {
+//     icon: CiBowlNoodles,
+//     name: "Pasta",
+//     items: 0,
+//   },
+//   {
+//     icon: LuIceCream2,
+//     name: "Desserts",
+//     items: 0,
+//   },
+//   {
+//     icon: LuSalad,
+//     name: "Salad",
+//     items: 0,
+//   },
+//   {
+//     icon: MdOutlineBakeryDining,
+//     name: "Baked",
+//     items: 0,
+//   },
+//   {
+//     icon: LuPopcorn,
+//     name: "Snacks",
+//     items: 0,
+//   },
+//   {
+//     icon: BiBowlRice,
+//     name: "Appetizers",
+//     items: 0,
+//   },
+//   {
+//     icon: IoFishOutline,
+//     name: "Seafood",
+//     items: 0,
+//   },
+// ];
 
 export default function Recipes() {
   const { getRecipesByCategory, getRecipesCount } = useGetRecipes();
