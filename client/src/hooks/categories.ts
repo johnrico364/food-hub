9915f2d15fa5a68853ffa2255786634ff3d0bf45
John Anthony Rico <johnrico364@gmail.com>
2024-11-08 22:06:19 +0800

    Add: Move Categories to another file

---
+++ client/src/hooks/categories.ts
@@ -0,0 +1,66 @@
+// Icons
+import { AiOutlineAppstoreAdd } from "react-icons/ai";
+import { CiBowlNoodles } from "react-icons/ci";
+import { IoFishOutline, IoRestaurantOutline } from "react-icons/io5";
+import { LiaMugHotSolid } from "react-icons/lia";
+import { LuIceCream2, LuPopcorn, LuSalad, LuSoup } from "react-icons/lu";
+import { MdOutlineBakeryDining } from "react-icons/md";
+import { BiBowlRice } from "react-icons/bi";
+
+export const categories = [
+  {
+    icon: AiOutlineAppstoreAdd,
+    name: "All",
+    items: 0,
+  },
+  {
+    icon: IoRestaurantOutline,
+    name: "Main course",
+    items: 0,
+  },
+  {
+    icon: LiaMugHotSolid,
+    name: "Breakfast",
+    items: 0,
+  },
+  {
+    icon: LuSoup,
+    name: "Soups",
+    items: 0,
+  },
+  {
+    icon: CiBowlNoodles,
+    name: "Pasta",
+    items: 0,
+  },
+  {
+    icon: LuIceCream2,
+    name: "Desserts",
+    items: 0,
+  },
+  {
+    icon: LuSalad,
+    name: "Salad",
+    items: 0,
+  },
+  {
+    icon: MdOutlineBakeryDining,
+    name: "Baked",
+    items: 0,
+  },
+  {
+    icon: LuPopcorn,
+    name: "Snacks",
+    items: 0,
+  },
+  {
+    icon: BiBowlRice,
+    name: "Appetizers",
+    items: 0,
+  },
+  {
+    icon: IoFishOutline,
+    name: "Seafood",
+    items: 0,
+  },
+];
