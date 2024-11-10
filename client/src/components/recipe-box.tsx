3b10ed737e5ccfea1f9ee669a7737fb917ba9aaa
John Anthony Rico <johnrico364@gmail.com>
2024-11-11 00:11:11 +0800

    Update: Adjust image size

--- client/src/components/recipe-box.tsx
+++ client/src/components/recipe-box.tsx
@@ -14,13 +14,13 @@ export function RecipeBox(data: Props) {
   return (
     <div className="recipe-box w-[14rem] h-[19.8rem]">
       <div className="card bg-base-100">
-        <figure className="px-3 mt-2 h-[150px] w-[225px]">
+        <figure className="px-3 mt-2 h-[150px] w-[13.6rem] relative overflow-hidden">
           <Image
-            className="rounded-xl object-cover"
+            className="rounded-xl object-contain"
             src={`http://127.0.0.1:8000/api/image/${data?.image}`}
             alt="Shoes"
-            height={120}
-            width={200}
+            layout="fill"
+            objectFit="cover"
           />
         </figure>
         <div className="recipe-details items-center">
