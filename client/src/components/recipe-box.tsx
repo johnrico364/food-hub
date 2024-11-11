345694017934fba4182cbc12e95defcdef2f105d
John Anthony Rico <johnrico364@gmail.com>
2024-11-11 15:56:04 +0800

    Update: make image more center

--- client/src/components/recipe-box.tsx
+++ client/src/components/recipe-box.tsx
@@ -14,7 +14,7 @@ export function RecipeBox(data: Props) {
   return (
     <div className="recipe-box w-[14rem] h-[19.8rem]">
       <div className="card bg-base-100">
-        <figure className="mt-2 ml-1 h-[150px] w-[13.4rem] relative overflow-hidden">
+        <figure className="mt-2 ml-[0.18rem] h-[150px] w-[13.4rem] relative overflow-hidden">
           <Image
             className="rounded-xl object-contain"
             src={`http://127.0.0.1:8000/api/image/${data?.image}`}
